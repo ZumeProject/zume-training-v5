ce2f74424cd1607ca81de1722a125c693141d768
Chris Chasm <chris@chasm.solutions>
2022-12-05 12:27:12 -0700

    test

--- tests/test_eslint.sh
+++ tests/test_eslint.sh
@@ -10,4 +10,5 @@ eval eslint \
     --ignore-pattern gulpfile.js \
     --ignore-pattern '*.min.js' \
     --ignore-pattern admin/multi-role/js/min/ \
+    --ignore-pattern assets/styles/foundation-sites \
     .
